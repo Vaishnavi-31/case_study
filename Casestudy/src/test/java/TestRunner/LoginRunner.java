5f678164b2cee6cae94d622c0d33dc19212f7896
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3C-1PHPGP2.dir.svc.accenture.com>
2020-01-29 06:11:49 -0600

    plugin added.

--- Casestudy/src/test/java/TestRunner/LoginRunner.java
+++ Casestudy/src/test/java/TestRunner/LoginRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features" , glue={ "casestudy" } , tags="@product")
+@CucumberOptions(features = "features" , glue={ "casestudy" } , tags="@product", plugin = {"pretty","json:target/cucumber.json"})
 public class LoginRunner {
 	
 }
